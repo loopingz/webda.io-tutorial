caf844bc0c967591ce928b695fa7b27357fd1669
Remi Cattiau <remi@cattiau.com>
2023-05-17 15:49:10 -0700

    feat: add authentication

--- src/models/contact.ts
+++ src/models/contact.ts
@@ -1,10 +1,10 @@
-import { CoreModel, Expose } from "@webda/core";
+import { Expose, OwnerModel } from "@webda/core";
 
 /**
  * Define here a model that can be used along with Store service
  */
 @Expose()
-export class Contact extends CoreModel {
+export class Contact extends OwnerModel {
   /**
    * First name of our contact
    */
@@ -24,8 +24,4 @@ export class Contact extends CoreModel {
    * Notes
    */
   notes: string;
-  async canAct(ctx, action: string): Promise<string | boolean> {
-    // Require user to be authenticated to do anything on contact
-    return ctx.getCurrentUserId() !== undefined;
-  }
 }
